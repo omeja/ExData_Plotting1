693885dbfa444ecbfcd4cd3e9fdc3ec0d9f457a9
osbert.osamai <osbert.osamai@nita.go.ug>
2015-06-07 21:33:40 +0300

    gg

--- plot1.R
+++ plot1.R
@@ -1,2 +1,9 @@
-data<-read.csv('household_power_consumption.txt',header=FALSE,nrows=2880,sep=";",skip=66637,na.strings='!')
-hist(data$V3,col="red",main="Global Active Power",xlab="Global Active Power(Kilowatts)")
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
